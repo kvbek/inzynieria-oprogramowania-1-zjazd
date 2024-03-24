b98965b0364fc1d8f24fb5692a829813464e94b8
Jakub Marciniak <jakub.s.marciniak@gmail.com>
2024-03-24 10:54:34 +0100

    Added trapezoid area

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ def square_area(a):
 print(square_area(bok_a))
 
 # Pole prostokata
-
 def triangle_area(a, h):
     return 0.5 * a * h
 
@@ -16,3 +15,11 @@ bok_a = 3
 bok_b = 7
 
 print(triangle_area(bok_a, bok_b))
+
+# Pole trapezu
+def trapezoid_area(a, b, h):
+    return 0.5 * (a + b) * h
+
+bok_a, bok_b, h, = 5, 10, 15
+
+print(trapezoid_area(bok_a, bok_b, h))
